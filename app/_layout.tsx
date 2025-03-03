a5ecc0e4bb0d7527d64692833e904a87b87ceb57
emmvega <emmanuel_vega_mecinas@outlook.com>
2025-03-02 19:22:57 -0600

    setting nativewind and colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,8 +5,10 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import "../global.css"
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Text } from 'react-native';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -29,10 +31,9 @@ export default function RootLayout() {
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
+      {/* <Stack> */}
+     <Text className='mt-10 text-7xl font-bold text-light-primary dark:text-dark-text'>Hola universo</Text>
+      {/* </Stack> */}
       <StatusBar style="auto" />
     </ThemeProvider>
   );
